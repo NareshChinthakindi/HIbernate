214c8f9de0164c037d1882befcb4b07262eec6b9
Naresh <naresh.chinthakindi@gmail.com>
2015-12-06 00:03:10 +0530

    Added annotation attributes

--- src/org/naresh/javariders/HibernateMain.java
+++ src/org/naresh/javariders/HibernateMain.java
@@ -3,6 +3,8 @@
  */
 package org.naresh.javariders;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -22,7 +24,10 @@ public class HibernateMain {
 		UserDetails userDetails = new UserDetails();
 
 		userDetails.setUserId(1);
-		userDetails.setUserName("Naresh");
+		userDetails.setUserName("Third Naresh");
+		userDetails.setAddress("Addre Naresh");
+		userDetails.setDescription("Description Naresh");
+		userDetails.setJoinedDate(new Date());
 
 		Configuration config =	new Configuration().configure();
 		SessionFactory sessionFactory = config.buildSessionFactory();
@@ -31,6 +36,19 @@ public class HibernateMain {
 		session.beginTransaction();
 		session.save(userDetails);
 		session.getTransaction().commit();
+		session.close();
+		
+		
+		userDetails = null;
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		userDetails = (UserDetails) session.get(UserDetails.class, 1);
+		
+		System.out.println(userDetails.getUserName());
+		
+		
+		
 	}
 
 }
--- src/org/naresh/javariders/dto/UserDetails.java
+++ src/org/naresh/javariders/dto/UserDetails.java
@@ -3,19 +3,70 @@
  */
 package org.naresh.javariders.dto;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * @author Naresh
  *
  */
 @Entity
+@Table(name="USER_DETAILS")
 public class UserDetails {
 
 	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	@Column(name="USER_NAME")
 	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String address;
+	@Lob
+	private String description;
+	/**
+	 * @return the joinedDate
+	 */
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	/**
+	 * @param joinedDate the joinedDate to set
+	 */
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	/**
 	 * @return the userId
 	 */
