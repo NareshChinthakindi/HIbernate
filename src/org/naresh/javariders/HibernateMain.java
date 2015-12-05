c61bbb104fdd3ae4a1d93da98a3d38e919639230
Naresh <naresh.chinthakindi@gmail.com>
2015-12-05 21:39:38 +0530

    Inital Check-in

---
+++ src/org/naresh/javariders/HibernateMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.naresh.javariders;
+
+/**
+ * @author Naresh
+ *
+ */
+public class HibernateMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
